ee27873e4bbd9abd34c0bd8add1a21bd14a5d721
Matheus Faleiro <matheus.faleiro@concrete.com.br>
2018-04-13 12:03:59 -0300

    Add Images To Movies List Adapter

--- app/src/main/java/theuzfaleiro/github/io/themoviedb/ui/feature/movies/MovieAdapter.kt
+++ app/src/main/java/theuzfaleiro/github/io/themoviedb/ui/feature/movies/MovieAdapter.kt
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_movie_layout.view.*
 import theuzfaleiro.github.io.themoviedb.R
 import theuzfaleiro.github.io.themoviedb.data.model.movie.Movie
@@ -23,16 +24,18 @@ class MovieAdapter(private val movieList: List<Movie> = listOf(), private val mo
 
     class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        private val movieName = itemView.textViewMovieName
+        private val moviePoster = itemView.imageViewMoviePoster
 
         fun bindItemsToView(movie: Movie, movieSelected: (movieSelected: Movie) -> Unit) {
 
-            movieName.text = movie.title
+            Glide.with(moviePoster.context)
+                    .load("https://image.tmdb.org/t/p/w780/noU2Im2GcF9TuqJcJUaU2peP5rO.jpg")
+                    .into(moviePoster)
 
             itemView.setOnClickListener {
                 movieSelected(movie)
             }
         }
-
     }
+
 }
